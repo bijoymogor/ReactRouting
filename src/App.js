cb45a66ebe5baca7ff07e84b403e4c5f4999f62a
Bijoy <bijoymogor@gmail.com>
2023-04-12 14:00:27 +0530

    Added Routes to different pages in latest and deprecated approaches

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createRoutesFromElements, Route} from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+
+
+//Paths -->
+//https//example.com/products
+//https//example.com/
+
+
+// React-Router-DOM package > 6.4 preferred way currently
+// const router = createBrowserRouter([
+//   {path: '/', element: <HomePage/>},
+//   {path: '/products', element: <Products/>}
+// ]);
+
+// React-Router-DOM package < 6.4 old way deprecated
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage/>}/>
+    <Route path="/products" element={<Products/>}/>
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+
+
 function App() {
-  return <div></div>;
+  return (
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App;
