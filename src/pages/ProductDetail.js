6ba718aa51ecc84e48c022ad567e647370d73017
Bijoy <bijoymogor@gmail.com>
2023-04-15 14:56:01 +0530

    Relative and Absolute Path, Added Back Link in ProductDetail

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 function ProductDetail() {
     const params = useParams();
@@ -10,6 +10,7 @@ function ProductDetail() {
         <>
         <h1>Product Detail</h1>
         <p>{params.productId}</p>
+        <p><Link to=".." relative="path">Back</Link></p>
         </>
     )
 }
