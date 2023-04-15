0139d957165446a0db4508b104c268032817e15c
Bijoy <bijoymogor@gmail.com>
2023-04-15 13:25:44 +0530

    Styling Nav Links with react-router-dom

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { NavLink } from 'react-router-dom';
+
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
@@ -8,8 +10,11 @@ function MainNavigation() {
         <header className={classes.header}>
             <nav>
                 <ul className={classes.list}>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/products'>Products</Link></li>
+                    {/* <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/products'>Products</Link></li> */}
+
+                    <NavLink to="/" className={({isActive}) => isActive ? classes.active : undefined } end>Home</NavLink>
+                    <NavLink to="/products" className={({ isActive }) => isActive ? classes.active : undefined} end>Products</NavLink>
                 </ul>
             </nav>
         </header>
